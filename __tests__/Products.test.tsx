6f48457a045182241ffe12656fa57705264ef229
mahtabfalahat <mahtab.falahatnezhad98@gmail.com>
2023-12-01 13:56:27 -0800

    add triple a in test case

--- __tests__/Products.test.tsx
+++ __tests__/Products.test.tsx
@@ -1,11 +1,22 @@
 import Products from "@/pages/products";
 import { expect, test } from "vitest";
 import { render, screen, within } from "@testing-library/react";
+import { ProductSummary } from "@/@types/product-summary.interface";
 
 test("it should be show empty state", () => {
+  //1. act
+  const allProudcts: ProductSummary[] = [];
+  const currentPage = 1;
+  const totalPages = 0;
+  //2. arrange
   const { container } = render(
-    <Products allProducts={[]} currentPage={1} totalPages={0} />
+    <Products
+      allProducts={allProudcts}
+      currentPage={currentPage}
+      totalPages={totalPages}
+    />
   );
+  //3. assert
   const paragraph = container.querySelector("p");
   expect(paragraph).toBeDefined();
   const contentResult = screen.findByText("no product");
